7d3ec47aabeb104cdf7e2fe2708921029c255b28
ArkSeyonet <31637812+ArkSeyonet@users.noreply.github.com>
2019-12-02 09:30:57 -0600

    Update _resource.lua with esx_voice dependency

--- __resource.lua
+++ __resource.lua
@@ -5,21 +5,22 @@ description 'ESX Identity'
 version '4.0.0'
 
 server_scripts {
-	'@mysql-async/lib/MySQL.lua',
+    '@mysql-async/lib/MySQL.lua',
     '@es_extended/locale.lua',
-	'locales/en.lua',
-	'config.lua',
-	'server/main.lua'
+    'locales/en.lua',
+    'config.lua',
+    'server/main.lua'
 }
 
 client_scripts {
     '@es_extended/locale.lua',
     'locales/en.lua',
-	'config.lua',
-	'client/main.lua'
+    'config.lua',
+    'client/main.lua'
 }
 
 dependencies {
     'es_extended',
-	'esx_skin'
+    'esx_skin',
+    'esx_voice'
 }
